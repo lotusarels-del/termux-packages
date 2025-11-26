102a385fe281b9eeb0f76de99bb2333703431de4
lotusarels-del <lotusarels@gmail.com>
2025-11-26 16:24:25 +0530

    Update build.sh

--- packages/libicu/build.sh
+++ packages/libicu/build.sh
@@ -1,16 +1,15 @@
 TERMUX_PKG_HOMEPAGE=http://site.icu-project.org/home
-TERMUX_PKG_DESCRIPTION='International Components for Unicode library'
+TERMUX_PKG_DESCRIPTION="International Components for Unicode library"
 TERMUX_PKG_LICENSE="custom"
-# We override TERMUX_PKG_SRCDIR termux_step_post_get_source so need to do
-# this hack to be able to find the license file.
+# We override TERMUX_PKG_SRCDIR in termux_step_post_get_source
 TERMUX_PKG_LICENSE_FILE="../LICENSE"
 TERMUX_PKG_MAINTAINER="@termux"
-# Never forget to always bump revision of reverse dependencies and rebuild them
-# when bumping "major" version.
+
 TERMUX_PKG_VERSION="77.1"
 TERMUX_PKG_REVISION=2
 TERMUX_PKG_SRCURL=https://github.com/unicode-org/icu/releases/download/release-${TERMUX_PKG_VERSION//./-}/icu4c-${TERMUX_PKG_VERSION//./_}-src.tgz
 TERMUX_PKG_SHA256=588e431f77327c39031ffbb8843c0e3bc122c211374485fa87dc5f3faff24061
+
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_UPDATE_METHOD=repology
 TERMUX_PKG_DEPENDS="libc++"
@@ -21,16 +20,16 @@ TERMUX_PKG_EXTRA_HOSTBUILD_CONFIGURE_ARGS="--disable-samples --disable-tests"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-samples --disable-tests --with-cross-build=$TERMUX_PKG_HOSTBUILD_DIR"
 
 termux_step_pre_configure() {
-  export CFLAGS="$CFLAGS -fPIC"
-  export CXXFLAGS="$CXXFLAGS -fPIC"
-  export LDFLAGS="$LDFLAGS -fPIC"
-  # Force ICU internal build system
-  export U_CFLAGS="-fPIC"
-  export U_CXXFLAGS="-fPIC"
+	export CFLAGS="$CFLAGS -fPIC"
+	export CXXFLAGS="$CXXFLAGS -fPIC"
+	export LDFLAGS="$LDFLAGS -fPIC"
+	export CPPFLAGS="$CPPFLAGS -fPIC"
+	export U_CFLAGS="-fPIC"
+	export U_CXXFLAGS="-fPIC"
 }
 
 termux_step_post_get_source() {
-	rm $TERMUX_PKG_SRCDIR/LICENSE
+	rm -f $TERMUX_PKG_SRCDIR/LICENSE
 	curl -o $TERMUX_PKG_SRCDIR/LICENSE -L https://raw.githubusercontent.com/unicode-org/icu/release-${TERMUX_PKG_VERSION//./-}/LICENSE
 	TERMUX_PKG_SRCDIR+="/source"
 	find . -type f | xargs touch
